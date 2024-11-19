f5e2ae5cbaadebf5f783fb2ae9767d5acca33dc9
lokesh1jha <lokesh1jha@gmail.com>
2024-11-19 23:25:13 +0530

    redirect fixed

--- components/auth/SignOutButton.tsx
+++ components/auth/SignOutButton.tsx
@@ -1,12 +1,20 @@
-'use client'
-import React from 'react'
-import { Button } from '@/components/ui/button'
-import { signOut } from 'next-auth/react'
+'use client';
+import React from 'react';
+import { Button } from '@/components/ui/button';
+import { signOut } from 'next-auth/react';
+import { Routes } from '@/routes';
 
 const SignOutButton = () => {
   return (
-    <Button variant="outline" onClick={async () => await signOut()}>Sign Out</Button>
-  )
-}
+    <Button
+      variant="outline"
+      onClick={async () => {
+        await signOut({ callbackUrl: Routes.LOGIN }); 
+      }}
+    >
+      Sign Out
+    </Button>
+  );
+};
 
-export default SignOutButton
+export default SignOutButton;
