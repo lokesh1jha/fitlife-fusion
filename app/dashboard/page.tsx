c9c3d02f28cadbe85d0b256832cc10334500d60a
lokesh1jha <lokesh1jha@gmail.com>
2024-11-21 00:37:02 +0530

    fix + db update

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -6,15 +6,16 @@ import OnboardingWizard from "@/components/onboarding/page";
 import { useState } from "react";
 
 export default function DashboardHome() {
-  const [onboardingCompleted, setOnboardingCompleted] = useState(false);
+  // const [onboardingCompleted, setOnboardingCompleted] = useState(false);
 
-  function handleOnboardingComplete() {
-    setOnboardingCompleted(true);
-  }
+  // function handleOnboardingComplete() {
+  //   setOnboardingCompleted(true);
+  // }
 
-  if(true) {
-    handleOnboardingComplete()
-  } 
+  // if(true) {
+  //   handleOnboardingComplete()
+  // } 
+  let onboardingCompleted = true
   return (
     <div>
       {onboardingCompleted ?
